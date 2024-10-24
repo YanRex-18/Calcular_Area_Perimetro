530a9625c726c1f141d0676b8b2352b030bbe211
YanRex-18 <yandri.macias8698@utc.edu.ec>
2024-10-23 20:56:43 -0500

    Create areayperimetro1.2.py

---
+++ areayperimetro1.2.py
@@ -0,0 +1,8 @@
+base=float(input("Ingrese la base del rectangulo: ",))
+altura=float(input("ingresa la altura del rectangulo: "),)
+
+area=base*altura
+perimetro=2*(base+altura)
+
+print("el area es: ",round(area,1))
+print("el perimetro es: ",round(perimetro,1))
