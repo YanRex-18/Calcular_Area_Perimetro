2e59b2ea6774821964e800964db516729cf96866
YanRex-18 <yandri.macias8698@utc.edu.ec>
2024-10-23 21:10:04 -0500

    Create area_perimetro1.2.py

---
+++ calcularcuadrado/area_perimetro1.2.py
@@ -0,0 +1,9 @@
+lado1=float(input("ingrese un lado del cuadrado: ",))
+lado2=float(input("ingrese otro lado del cuadrado: "),)
+lado3=float(input("ingrese un lado del cuadrado: ",))
+lado4=float(input("ingrese otro lado del cuadrado: "),)
+area=lado1*lado2
+
+perimetro=lado1+lado2+lado3+lado4
+print("el area es: ",round(area,1),"cm^2")
+print("el perimetro es: ",round(perimetro,1))
